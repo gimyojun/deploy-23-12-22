45b92b9c2c101db3a0ffad8768523d2b02dfa822
gimyojun <gywnsdl5623@gmail.com>
2023-12-22 05:31:32 +0900

    젠킨스 빌드 테스트 5(테스트 실패 테스트 2)

--- src/test/java/com/ll/deploy231219/Deploy231219ApplicationTests.java
+++ src/test/java/com/ll/deploy231219/Deploy231219ApplicationTests.java
@@ -3,14 +3,14 @@ package com.ll.deploy231219;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
+import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @ActiveProfiles("test")
 class Deploy231219ApplicationTests {
 
     @Test
     void contextLoads() {
-
+        assertThat(1).isEqualTo(2);
     }
 
 }
